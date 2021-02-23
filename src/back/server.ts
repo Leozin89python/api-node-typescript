82cd224357e5949ddf829d045d65f6260ce6019e
Leozin89Python <leozin069@gmail.com>
2021-02-23 11:03:56 -0300

    api-node-typescript-version-1.1.6

--- src/back/server.ts
+++ src/back/server.ts
@@ -1,12 +1,10 @@
 const port :number = 3004
 
+import routes from './routes/route'
+
 import express from 'express'
 const app = express()
 
-app.use('/',(req,res)=>{
-	res.json({
-		msg:'online'
-	})
-})
+app.use(routes)
 
 app.listen(port)
